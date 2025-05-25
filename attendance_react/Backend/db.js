db027bb8d7a7bd7770c3c436f795c0a26ca6e9ac
Sourabhgupta-11 <sourabhgupta.1331@gmail.com>
2025-05-25 14:55:14 +0530

    Setting up backend, Create server and setting database

---
+++ attendance_react/Backend/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURL = process.env.MONGODB_URL_LOCAL; 
+/*const mongoURL=process.env.MONGODB_URL*/
+
+mongoose.connect(mongoURL)
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+db.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
+
+db.on('error', (err) => {
+  console.log('Error opening MongoDB connection:', err);
+});
+
+
+module.exports=db;
---
+++ attendance_react/Backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const db=require('./db')
+
+const authRoutes = require('./Routes/auth');
+const studentRoutes = require('./Routes/Student');
+const teacherRoutes = require('./Routes/Teacher');
+const adminRoutes = require('./Routes/admin');
+const attendanceRoutes = require('./Routes/Attendance');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api/auth', authRoutes);
+app.use('/api/students', studentRoutes);
+app.use('/api/teachers', teacherRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/attendance', attendanceRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
