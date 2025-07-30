d7e2577d1431c90f4ad766e4406e94b1495b0778
Sourabhgupta-11 <sourabhgupta.1331@gmail.com>
2025-07-30 16:02:34 +0530

    Organise structure of file

--- 1st page/script.js
+++
@@ -1,6 +0,0 @@
-let isStudent = true;
-function toggleRole() {
-    const toggleSlider = document.querySelector(".toggle-slider");
-    isStudent = !isStudent;
-    toggleSlider.style.transform = isStudent ? "translateX(0%)" : "translateX(100%)";
-}
